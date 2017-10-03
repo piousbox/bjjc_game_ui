eeda61c93877135081e242cc95a8b5208b090517
piousbox <piousbox@gmail.com>
2017-10-02 19:47:57 -0700

    render a badge on the map

--- app/components/Locations/LocationShow.jsx
+++ app/components/Locations/LocationShow.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import { Link } from 'react-router'
+import Draggable from 'react-draggable'
 
 import { setLocation } from '../../actions'
 
-import Draggable from 'react-draggable'
 
 class LocationShow extends React.Component {
 
@@ -16,11 +17,26 @@ class LocationShow extends React.Component {
   render () {
     console.log("+++ +++ LocationShow render:", this.props, this.state)
 
-    let oWidth = 200;
-    let oHeight = 200;
+    let oWidth = 200
+    let oHeight = 200
     if (document.getElementById('web-design-6')) {
       oWidth = document.getElementById('web-design-6').offsetWidth
       oHeight = document.getElementById('web-design-6').offsetHeight
+    }
+
+    let badges = []
+    let badgesKey = 0
+    if (this.props.location.badges) {
+      this.props.location.badges.map((badge) => {
+        badges.push(
+          <Link to="#" key={badgesKey++} >
+            <div className="badge"
+                 style={{ position: 'absolute', top: badge.bg_pos_y, left: badge.bg_pos_x,
+                          width: '100px', height: '100px', display: 'block',
+                          background: `url(${badge.shaded_photo})`
+                 }} />
+          </Link>)
+      })
     }
 
     return(
@@ -33,7 +49,9 @@ class LocationShow extends React.Component {
         <Draggable bounds="parent" >
           <div id="locationMap" style={{ height: this.props.location.background_image_height,
                                          width: this.props.location.background_image_width,
-                                         background: `no-repeat url(${this.props.location.background_image_path})` }}>
+                                         background: `no-repeat url(${this.props.location.background_image_path})`,
+                                         position: 'relative' }}>
+            { badges }
           </div>
         </Draggable>
       </div>
