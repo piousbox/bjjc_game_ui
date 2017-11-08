678f239e217b35e10eaa222b53407f2702b2da61
piousbox <piousbox@gmail.com>
2017-11-08 14:44:47 -0800

    come on

--- config/production/config/index.jsx
+++ config/production/config/index.jsx
@@ -5,6 +5,7 @@ let Config = {
   fbAppId: "333229727087961",
   domain: "gameui.piousbox.com",
   siteTitle: "BJJC GAMEUI",
+  stripePublicKey: "pk_live_p5vWmspvkVlBNPPVCvf4Rtg9"
 }
 
 export default Config
